da3b031df057753301e1b445bed9ae265d8675a9
ferelx <web@fernandobernal.es>
2013-02-06 12:29:00 +0100

    cambios css para pricing

--- templates/node--species-mini-teaser.tpl.php
+++
@@ -1,9 +0,0 @@
-<a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a>
- 
-<?php
-  if (!empty($content['field_sp_descr_notes'])) {
-    print '(this is the mini-teaser view mode)';
-    print '<div class="description">' . render($content['field_sp_genus']) . '</div>';
-  }  
-  print '<div class="thumbnail">' . views_embed_view('species_figure','block_4', $element['#object']->nid) . '</div>';
-?>
--- templates/zone--user.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php if (!empty($_SESSION['global_filter']['view_taxo_paisos'])): ?><div id="active-country"><?php endif; ?>
-<?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>
-  <div<?php print $content_attributes; ?>>
-    <?php print $content; ?>
-  </div>
-<?php if ($wrapper): ?></div><?php endif; ?>
-<?php if (!empty($_SESSION['global_filter']['view_taxo_paisos'])): ?></div><?php endif; ?>
