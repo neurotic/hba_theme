f4811d6a4d73e44e4cbcd9965ca41edef820e541
Carlos Rincón <CarlosRincon@MacBook-Pro-de-Carlos.local>
2013-02-19 12:09:31 +0100

    Cambios en platees para que sea líquido y para mostrar y ocultar filtros (aún no sale nada que indique al usuario que puede hacerlo)

---
+++ ckeditor.styles.js
@@ -0,0 +1,52 @@
+/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+/*
+ * This file is used/requested by the 'Styles' button.
+ * The 'Styles' button is not enabled by default in DrupalFull and DrupalFiltered toolbars.
+ */
+if(typeof(CKEDITOR) !== 'undefined') {
+    CKEDITOR.addStylesSet( 'drupal',
+    [
+            /* Block Styles */
+
+            // These styles are already available in the "Format" drop-down list, so they are
+            // not needed here by default. You may enable them to avoid placing the
+            // "Format" drop-down list in the toolbar, maintaining the same features.
+            /*
+            { name : 'Paragraph'		, element : 'p' },
+            { name : 'Heading 1'		, element : 'h1' },
+            { name : 'Heading 2'		, element : 'h2' },
+            { name : 'Heading 3'		, element : 'h3' },
+            { name : 'Heading 4'		, element : 'h4' },
+            { name : 'Heading 5'		, element : 'h5' },
+            { name : 'Heading 6'		, element : 'h6' },
+            { name : 'Preformatted Text', element : 'pre' },
+            { name : 'Address'			, element : 'address' },
+            */
+
+            /*{ name : 'Blue Title'		, element : 'h3', styles : { 'color' : 'Blue' } },
+            { name : 'Red Title'		, element : 'h3', styles : { 'color' : 'Red' } },*/
+
+            /* Inline Styles */
+
+            // These are core styles available as toolbar buttons. You may opt enabling
+            // some of them in the "Styles" drop-down list, removing them from the toolbar.
+            /*
+            { name : 'Strong'			, element : 'strong', overrides : 'b' },
+            { name : 'Emphasis'			, element : 'em'	, overrides : 'i' },
+            { name : 'Underline'		, element : 'u' },
+            { name : 'Strikethrough'	, element : 'strike' },
+            { name : 'Subscript'		, element : 'sub' },
+            { name : 'Superscript'		, element : 'sup' },
+            
+
+            { name : 'Marker: Yellow'	, element : 'span', styles : { 'background-color' : 'Yellow' } },*/
+
+            { name : 'Multiple Link'	, element : 'span', attributes : { 'class' : 'lm-wrapper' } }
+            /*{ name : 'Multiple Link'	, element : 'span', attributes : { 'class' : 'lm-wrapper', 'style' : 'background: url(	/sites/all/themes/hbw/images/video.gif) no-repeat left 3px #EEE; padding-left: 15px' } }*/
+
+    ]);
+}
--- js/actions.js
+++ js/actions.js
@@ -6,31 +6,36 @@
 		    $(this).parent().parent().children('#edit-inputs').children('.fieldset-wrapper').toggleClass('visible');
 		    return false;
     	});*/
-    	
+
     	/*
     	$('#block-global-filter-global-filter-1').hover(
         function () {
           $(this).find('.fieldset-wrapper').toggleClass('visible');
-        }, 
+        },
         function () {
           $(this).find('.fieldset-wrapper').toggleClass('visible');
         }
       );*/
-      
+
       function toogleVisible() {
         $(this).find('.fieldset-wrapper, .content').toggleClass('visible');
       }
-            
-       var config = {    
-           over: toogleVisible, // function = onMouseOver callback (REQUIRED)    
-           timeout: 350, // number = milliseconds delay before onMouseOut    
-           out: toogleVisible // function = onMouseOut callback (REQUIRED)    
-      };    
+
+       var config = {
+           over: toogleVisible, // function = onMouseOver callback (REQUIRED)
+           timeout: 350, // number = milliseconds delay before onMouseOut
+           out: toogleVisible // function = onMouseOut callback (REQUIRED)
+      };
       $('#block-global-filter-global-filter-1').hoverIntent(config);
       $('#block-views-flag-nodes-block-5').hoverIntent(config);
-      $('#block-views-flag-nodes-block-6').hoverIntent(config);      
-    }
-  };
+      $('#block-views-flag-nodes-block-6').hoverIntent(config);
+
+      $('body.page-plate h1#page-title').click(function() {
+        //$('body.page-plate #region-content .view-filters').show();
+        $('body.page-plate #region-content .view-filters').slideToggle("slow");
+      });
+  }
+ };
 
 }(jQuery));
 
